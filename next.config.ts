b729e582c174ec68c515faef3d2d5d8ff5bb6a8e
Thanarit Kanjanametawat <thanarit.bonus@gmail.com>
2024-12-04 12:37:21 +0700

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    RUNPOD_ENDPOINT: process.env.RUNPOD_ENDPOINT,
+  }
 };
 
 export default nextConfig;
